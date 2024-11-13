5d81733f447b57cfe548d5d9b831dd6959e1c770
Jorge Matos <jisapjisap@gmail.com>
2024-11-13 17:54:03 +0100

    createAdmin

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono';
 import { zValidator } from "@hono/zod-validator"
 import { loginSchema, registerSchema } from '../schemas';
-
+import { createAdminClient } from '@/lib/appwrite';
+import { ID } from 'node-appwrite';
+import { setCookie } from 'hono/cookie';
+import { AUTH_COOKIE } from '../constants';
 
 
 const app = new Hono()
@@ -19,8 +22,30 @@ const app = new Hono()
     zValidator("json", registerSchema),
     async (c) => {
       const { name, email, password } = c.req.valid("json");
-      console.log({ name, email, password });
-      return c.json({name, email, password })
+      
+      const { account } = await createAdminClient();
+      const user = await account.create(
+        ID.unique(),
+        email,
+        password,
+        name,
+      );
+
+      const session = await account.createEmailPasswordSession(
+        email,
+        password,
+      );
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30,
+      });
+
+
+      return c.json({ data: user });
     }
   )
 
