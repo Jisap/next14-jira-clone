25183090fca31c802309980deade3c606210a6cf
Jorge Matos <jisapjisap@gmail.com>
2024-11-20 16:10:15 +0100

    handling image upload

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { createWorkspaceSchema } from "../schemas";
-import { DATABASE_ID, WORKSPACE_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACE_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 
@@ -16,15 +16,32 @@ const app = new Hono()
       const databases = c.get("databases")                         // Obtiene el databases del contexto (establecido en el middleware)
       const user = c.get("user")                                   // Obtiene el user del contexto (establecido en el middleware)
 
-      const { name } = c.req.valid("json")                         // Se valida el request (nombre del workspace)
+      const { name, image } = c.req.valid("json")                  // Se valida el request (nombre del workspace y la imagen) segun su esquema
       
+      let uploadedImageUrl: string | undefined;                    // Definimos una variable que almacenará la URL de la imagen subida
+      const storage = c.get("storage")                             // Se obtiene el storage del contexto (establecido en el middleware)
+
+      if(image instanceof File) {                                  // Si la imagen es un objeto File
+        const file = await storage.createFile(                     // Se crea el archivo en la base de datos de Appwrite
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image,
+        );
+        const arrayBuffer = await storage.getFilePreview(          // Se obtiene la vista previa del archivo
+          IMAGES_BUCKET_ID,
+          file.$id,
+        );
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`; // Se crea la URL de la imagen subida
+      }                                  
+
       const workspace = await databases.createDocument(            // Se crea el workspace en la base de datos
         DATABASE_ID,
         WORKSPACE_ID,
         ID.unique(),
         {
           name,
-          userId: user.$id
+          userId: user.$id,
+          imageUrl: uploadedImageUrl,
         }
       )
 
