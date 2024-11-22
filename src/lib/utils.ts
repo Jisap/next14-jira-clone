375e3bec3306b62885ca3e467b41696a24d54ebf
Jorge Matos <jisapjisap@gmail.com>
2024-11-22 17:18:09 +0100

    generateCode

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateIniteCode(length: number) {
+  const possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let code = ""
+  for (let i = 0; i < length; i++) {
+    code += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length))
+  }
+  return code
+}
