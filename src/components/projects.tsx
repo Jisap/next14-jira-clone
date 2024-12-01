9f18b9d5ed09dcf689ae907d7a38054da0288a3f
Jorge Matos <jisapjisap@gmail.com>
2024-12-01 16:06:21 +0100

    endpoint to create project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,9 +1,21 @@
 "use client"
 
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hook/use-workspace-id";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import { RiAddCircleFill } from "react-icons/ri"
 
 
 export const Projects = () => {
+
+  const projectId = null;                                                   // TODO: Use the projectId hook
+  const  pathname  = usePathname();
+  const workspaceId = useWorkspaceId(); // id desde la url
+  const { data } = useGetProjects({ workspaceId });
+
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
@@ -15,6 +27,22 @@ export const Projects = () => {
           onClick={() => {}} 
         />
       </div>
+      {data?.documents.map((project) => {
+        const href = `/workspaces/${workspaceId}/projects/${project.$id}`
+        const isActive = pathname === href
+        return (
+          <Link href={href} key={project.$id}>
+            <div className={cn(
+              "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500",
+              isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
+            )}>
+              <span className="truncate">
+                {project.name}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,12 +1,67 @@
-import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { z } from "zod";
+import { createProjectSchema } from "../schema";
 
 const app = new Hono()
+  .post(
+    "/",
+    sessionMiddleware,
+    zValidator("form", createProjectSchema),
+    async (c) => {
+      const databases = c.get("databases")                         // Obtiene el databases del contexto (establecido en el middleware)
+      const user = c.get("user")                                   // Obtiene el user del contexto (establecido en el middleware)
+
+      const { name, image, workspaceId } = c.req.valid("form")     // Se valida el request (nombre del proyecto, la imagen y el workspaceId) según su esquema
+
+      const member = await getMember({                             // Se comprueba si el usuario es miembro del workspace
+        databases,
+        workspaceId,
+        userId: user.$id,
+      })
+
+      if(!member){
+        return c.json({ error: "Unauthorized" }, 401)              // Si el usuario no es miembro del workspace se retorna un error
+      }
+
+
+      let uploadedImageUrl: string | undefined;                    // Definimos una variable que almacenará la URL de la imagen subida
+      const storage = c.get("storage")                             // Se obtiene el storage del contexto (establecido en el middleware)
+
+      if (image instanceof File) {                                 // Si la imagen es un objeto File
+        const file = await storage.createFile(                     // Se crea el archivo en la base de datos de Appwrite
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image,
+        );
+        const arrayBuffer = await storage.getFilePreview(          // Se obtiene la vista previa del archivo
+          IMAGES_BUCKET_ID,
+          file.$id,
+        );
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`; // Se crea la URL de la imagen subida
+      }
+
+      const project = await databases.createDocument(              // Se crea el projecto en la base de datos
+        DATABASE_ID,
+        PROJECTS_ID,
+        ID.unique(),
+        {
+          name,                                                    // y se almacena el nombre del workspace
+          userId: user.$id,                                        // y el ID del user que lo creó
+          imageUrl: uploadedImageUrl,                              // y la URL de la imagen subida (avatar)
+          workspaceId                                              // y el workspace al que pertenece
+        }
+      );
+
+      
+
+      return c.json({ data: project })
+    }
+  )
   .get(
     "/",
     sessionMiddleware,
