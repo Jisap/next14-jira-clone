81e9d742cd6843954a1d1a6b21e0059726093520
Jorge Matos <jisapjisap@gmail.com>
2024-12-02 16:10:19 +0100

    error & loading page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,12 +6,12 @@ import Link from "next/link"
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen flex flex-col gap-y-2 items-center justify-center">
-      <AlertTriangle className=""/>
-      <p className="text-sm text-muted-foreground">
+    <div className="h-screen flex flex-col gap-y-4 items-center justify-center">
+      <AlertTriangle className="size-6"/>
+      <p className="text-sm">
         Something went wrong
       </p>
-      <Button variant="secondary">
+      <Button variant="secondary" size="sm">
         <Link href="/">
           Back to home
         </Link>
