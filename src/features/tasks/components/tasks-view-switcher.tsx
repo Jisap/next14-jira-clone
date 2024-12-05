060fabb2c294c6ed983742664ca9c10d31ced578
Jorge Matos <jisapjisap@gmail.com>
2024-12-05 16:44:14 +0100

    useQueryState

--- src/features/tasks/components/tasks-view-switcher.tsx
+++ src/features/tasks/components/tasks-view-switcher.tsx
@@ -8,17 +8,25 @@ import { PlusIcon } from "lucide-react"
 import { useCreateTaskModal } from "../hooks/use-create-taks-modal"
 import { useGetTasks } from '../api/use-get-task';
 import { useWorkspaceId } from "@/features/workspaces/hook/use-workspace-id"
+import { useQueryState } from "nuqs"
 
 
 
 export const TasksViewSwitcher = () => {
 
+  const [view, setView] = useQueryState("tasksView", {
+    defaultValue: "table",
+  })
   const workspaceId = useWorkspaceId()
   const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({ workspaceId })
   const { open } = useCreateTaskModal();
 
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs 
+      defaultValue={view}
+      onValueChange={setView}
+      className="flex-1 w-full border rounded-lg"
+    >
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
@@ -58,10 +66,10 @@ export const TasksViewSwitcher = () => {
             {JSON.stringify(tasks)}
           </TabsContent>
           <TabsContent value="kanban" className="mt-0">
-            Data Kamban
+            {JSON.stringify(tasks)}
           </TabsContent>
           <TabsContent value="calendar" className="mt-0">
-            Data Calendar
+            {JSON.stringify(tasks)}
           </TabsContent>
         </>
       </div>
