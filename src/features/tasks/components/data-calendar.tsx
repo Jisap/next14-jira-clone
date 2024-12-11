3efb318cf719d157e3b3eead0d36a17de6aebb63
Jorge Matos <jisapjisap@gmail.com>
2024-12-11 15:52:01 +0100

    Calendar Styles

--- src/features/tasks/components/data-calendar.tsx
+++ src/features/tasks/components/data-calendar.tsx
@@ -10,13 +10,14 @@ import {
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import { enUS } from "date-fns/locale";
 import { useState } from "react";
-import { start } from "repl";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./data-calendar.css"
 
-const locales = {
+const locales = { //  Mapa de localizaciones, en este caso para el idioma inglés.
   "en-US": enUS
 }
 
-const localizer = dateFnsLocalizer({
+const localizer = dateFnsLocalizer({ // "localizador" para que el calendario sepa cómo manejar las fechas con la configuración de date-fns.
   format,
   parse,
   startOfWeek,
@@ -34,18 +35,40 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
     data.length > 0 ? new Date(data[0].dueDate) : new Date()
   );
 
-  const events = data.map((task) => ({
+  const events = data.map((task) => ({ // Transforma las tareas (data) en un formato que entiende react-big-calendar
     start: new Date(task.dueDate),
     end: new Date(task.dueDate),
     title: task.name,
     project: task.project,
     assignee: task.assignee,
     status: task.status,
-  }))
+    id: task.$id,
+  }));
+
+  const handleNavigate = (action: "PREV" | "NEXT" | "TODAY") => { // Permite navegar en el calendario a través de acciones predefinidas.
+    if (action === "PREV") {
+      setValue(subMonths(value, 1));
+    } else if (action === "NEXT") {
+      setValue(addMonths(value, 1));
+    } else if (action === "TODAY") {
+      setValue(new Date());
+    }
+  }
 
   return (
-    <div>
-
-    </div>
+    <Calendar 
+      localizer={localizer}
+      date={value}
+      events={events}
+      views={["month"]}
+      defaultView="month"
+      toolbar
+      showAllEvents
+      className="h-full"
+      max={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
+      formats={{
+        weekdayFormat: (date, culture, localizer) => localizer?.format(date, "EEE", culture) ?? "",
+      }}
+    />
   )
 }
--- src/features/tasks/components/tasks-view-switcher.tsx
+++ src/features/tasks/components/tasks-view-switcher.tsx
@@ -119,7 +119,7 @@ export const TasksViewSwitcher = () => {
                 onChange={onKambanChange}
               />
             </TabsContent>
-            <TabsContent value="calendar" className="mt-0">
+            <TabsContent value="calendar" className="mt-0 h-full pb-4">
               <DataCalendar data={tasks?.documents ?? []} />
             </TabsContent>
           </>
