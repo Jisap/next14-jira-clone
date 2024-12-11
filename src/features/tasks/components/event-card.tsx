918c6656db135a337babdfbe8a31e56e03c4d1ba
Jorge Matos <jisapjisap@gmail.com>
2024-12-11 16:37:32 +0100

    EventCard

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -3,6 +3,10 @@ import { TaskStatus } from "../types"
 import { cn } from "@/lib/utils"
 import { MemberAvatar } from "@/features/members/components/member-avatar"
 import { ProjectAvatar } from "@/features/projects/components/project-avatar"
+import { useWorkspaceId } from "@/features/workspaces/hook/use-workspace-id"
+import { useRouter } from "next/navigation"
+import React from "react"
+
 
 
 
@@ -24,12 +28,23 @@ const statusColorMap: Record<TaskStatus, string> = {
 }
 
 export const EventCard = ({ id, title, project, assignee, status }: EventCardProps) => {
+  
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+
+  const onClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    router.push(`/workspaces/${workspaceId}/tasks/${id}`);
+  }
+  
   return (
     <div className="px-2">
-      <div className={cn(
-        "p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition",
-        statusColorMap[status]
-      )}>
+      <div
+        onClick={onClick} 
+        className={cn(
+          "p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition",
+          statusColorMap[status]
+        )}>
         <p>
           {title}
         </p>
