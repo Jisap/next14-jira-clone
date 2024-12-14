508e8267d6a31c858010bdfe7e4b1bb8743ac440
Jorge Matos <jisapjisap@gmail.com>
2024-12-14 16:37:01 +0100

    analytics

---
+++ src/components/analytics-card.tsx
@@ -0,0 +1,16 @@
+
+
+interface AnalyticsCardProps {
+  title: string;
+  value: number;
+  variant: "up" | "down";
+  increaseValue: number;
+}
+
+export const AnalyticsCard = ({ title, value, variant, increaseValue }: AnalyticsCardProps) => {
+  return (
+    <div>
+      AnalyticsCard
+    </div>
+  )
+}
