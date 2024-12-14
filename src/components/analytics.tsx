be0790fd53a9fa797cbcb157d9e5ca7efe159a28
Jorge Matos <jisapjisap@gmail.com>
2024-12-14 16:36:21 +0100

    analytics

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,5 +1,6 @@
 import { ProjectAnalyticsResponseType } from '@/features/projects/api/use-get-project-analytics';
 import { ScrollArea } from './ui/scroll-area';
+import { AnalyticsCard } from './analytics-card';
 
 
 
@@ -9,7 +10,16 @@ export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
 
   return (
     <ScrollArea className="border rounded-lg w-full whitespace-nowrap shrink-0">
-
+      <div className='w-full flex flex-row'>
+        <div className='flex items-center flex-1'>
+          <AnalyticsCard 
+            title= "Total tasks"
+            value={data.taskCount}
+            variant={data.taskDifference > 0 ? "up" : "down"}
+            increaseValue={data.taskDifference}
+          />
+        </div>
+      </div>
     </ScrollArea>
   )
 }
